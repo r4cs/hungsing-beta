75fd59df3f51f1567ee89eace72bff43163ddd93
Raquel <r.guzansky@hotmail.com>
2020-03-12 16:43:29 -0300

    dealing with already login at login page

--- notificadorHSproject/users/views.py
+++ notificadorHSproject/users/views.py
@@ -126,7 +126,7 @@ def resend_confirmation():
 @users.route("/logout")
 @login_required
 def logout():
-    logout_user(current_user)
+    logout_user()
     return redirect(url_for('core.index'))
 
 
